e0cb2e54a16a343ae33944ad065d5e63070d5e37
lj <i@iamlj.com>
2019-08-30 10:39:54 +0800

    Auto commit

--- Demo/app.js
+++ Demo/app.js
@@ -1,13 +1,13 @@
 import { checkPermission } from "./common/utils";
 
 App({
-  roomName: undefined,
-  appid: 'd8lk7l4ed',
-  userId: undefined,
-  roomToken: undefined,
-  url: 'wss://rtmpgate.cloudvdn.com/',
+    roomName: undefined,
+    appid: 'ead903g53',
+    userId: undefined,
+    roomToken: undefined,
+    url: 'wss://rtmpgate.cloudvdn.com/',
 
-  onLaunch: function () {
-    checkPermission()
-  },
-})
+    onLaunch: function () {
+        checkPermission()
+    },
+});
